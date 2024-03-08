eda7dced2a34a1281af6965bd12dedeb72d9c5f0
Luca Bernstein <luca@lucabernstein.com>
2024-03-08 17:42:21 +0100

    Add parsing benchmarking test and add to Actions

--- input_test.go
+++ input_test.go
@@ -33,3 +33,15 @@ func TestParseInputArgs(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, []Interval{{Start: 1, End: 2}, {Start: 3, End: 4}}, output)
 }
+
+func BenchmarkParseInputArgs(b *testing.B) {
+	var intervals []Interval
+	var err error
+	for n := 0; n < b.N; n++ {
+		intervals, err = ParseInputArgs("[7,18][64000,128000][1,100000][2112,4224][0,1][25,117][1998,2401][2024,2025][15,20][117122,122117]")
+	}
+	// Assert recorded values from benchmarking test to avoid compiler optimizations (and possibly elimination of the function at test)
+	if err != nil {
+		b.Errorf("error in function while running benchmark: %s, intervals: %v", err.Error(), intervals)
+	}
+}
