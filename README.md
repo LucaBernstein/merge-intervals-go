242877bd61168193839820d01d4fca2f7e8f2b26
Luca Bernstein <luca@lucabernstein.com>
2024-03-09 16:42:42 +0100

    Add memory profile to benchmark, collect test coverage

--- README.md
+++ README.md
@@ -22,7 +22,19 @@ go run . "[25,30] [2,19] [14,23] [4,8]"
 
 ## Performance and Footprint
 
-> TODO...
+This package includes benchmark tests that show how different parts behave with different loads (input parsing, interval merge).
+
+Sample output of `go test -bench=. -run=XXX -benchmem`:
+
+```
+BenchmarkParseInputArgs-12    1254063      977.8 ns/op    1136 B/op    21 allocs/op
+BenchmarkMerge-12             9526440      120.7 ns/op     312 B/op     6 allocs/op
+BenchmarkMerge1000-12         1798074      661.0 ns/op      88 B/op     3 allocs/op
+BenchmarkMerge10000-12         192378     6181.0 ns/op      88 B/op     3 allocs/op
+BenchmarkMerge100000-12         19362    61873.0 ns/op      88 B/op     3 allocs/op
+PASS
+ok      github.com/LucaBernstein/merge-intervals-go     8.281s
+```
 
 ## Considerations and Assumptions
 
--- merge_test.go
+++ merge_test.go
@@ -34,3 +34,42 @@ func TestMerge(t *testing.T) {
 	// Merge intervals with negative numbers
 	assert.Equal(t, []Interval{{Start: -11, End: -3}, {Start: -1, End: 1}}, Merge([]Interval{{Start: -11, End: -3}, {Start: -1, End: 0}, {Start: 0, End: 1}}))
 }
+
+func BenchmarkMerge(b *testing.B) {
+	sampleInput, _ := ParseInputArgs("[0,1][1,2][2,3][3,100][4,101][5,102][600,700][800,999][1000,1000][1001,1002][1002,1004][1003,10000]")
+	var intervals []Interval
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		intervals = Merge(sampleInput)
+	}
+	b.StopTimer()
+	formattedOutput := FormatOutput(intervals)
+	if formattedOutput != "[0,102] [600,700] [800,999] [1000,1000] [1001,10000]" {
+		b.Errorf("unexpected output: %s", formattedOutput)
+	}
+}
+
+func fillIntersectingInterval(size int) (sample []Interval) {
+	for i := range size {
+		sample = append(sample, Interval{Start: i, End: i + 2})
+	}
+	return
+}
+
+func benchmarkMergeSkeleton(intervalCount int, b *testing.B) {
+	sampleInput := fillIntersectingInterval(intervalCount)
+	var intervals []Interval
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		intervals = Merge(sampleInput)
+	}
+	b.StopTimer()
+	expectedInterval := Interval{Start: 0, End: intervalCount + 1}
+	if len(intervals) != 1 || intervals[0] != expectedInterval {
+		b.Errorf("unexpected output: %v", intervals)
+	}
+}
+
+func BenchmarkMerge1000(b *testing.B)   { benchmarkMergeSkeleton(1000, b) }
+func BenchmarkMerge10000(b *testing.B)  { benchmarkMergeSkeleton(10000, b) }
+func BenchmarkMerge100000(b *testing.B) { benchmarkMergeSkeleton(100000, b) }
